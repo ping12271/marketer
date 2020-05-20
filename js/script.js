909d28249528f3179521fcdf99ca6fb4e2e260ae
shin hyeeun <ping12271@hanmail.net>
2020-05-21 01:04:20 +0900

    jquery 수정

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-$('.main-footer,.gallery').on('click', function () {
-    return
+$('.gallery a').on('click', function () {
+    $('.popup').addClass('show');
 })
+
+$('.popup').on('click', function () {
+    $('.popup').removeClass('show')
+})
+
+
+//toggle//
+$('.que').on('click', function(){
+    $(this).closest('li').toggleClass('is_active')
+    $(this).next('.answer').slideToggle();
+})
+
+//header//
+
