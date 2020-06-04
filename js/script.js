666086aebf0a023200445c7bedf84c3b396fcfdb
shin hyeeun <ping12271@hanmail.net>
2020-06-04 15:53:12 +0900

    ..

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
-$('.gallery a').on('click', function () {
-    $('.popup').addClass('show');
-})
-
-$('.popup').on('click', function () {
-    $('.popup').removeClass('show')
-})
-
-
-//toggle//
 $('.que').on('click', function(){
-    $(this).closest('li').toggleClass('is_active')
-    $(this).next('.answer').slideToggle();
+        $(this).closest('li').toggleClass('is_active')
+        $(this).next('.answer').slideToggle();
 })
 
-//header//
-$(function () {
-    $('.nav-item .nav-link').on('click', function () {
-        $('.nav-item .links').slideToggle(200);
-        $('.nav-item .nav-link .icon').toggleClass('show');
-    })
-})
 
