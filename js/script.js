908eb9f226c16c35b82c6b4a5d0d55872bf4b4e8
shin hyeeun <ping12271@hanmail.net>
2020-06-05 02:37:27 +0900

    slick추가

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-$('.que').on('click', function(){
-        $(this).closest('li').toggleClass('is_active')
-        $(this).next('.answer').slideToggle();
+$(function () {
+        /**
+         * marketer UI Script Initialize.
+         */
+        marketer.init();
 })
 
+const marketer = {
+        init: function () {
+                marketer.toggleSlideMenu()
+                marketer.getScroll()
+                marketer.backTop()
+                marketer.visualSlide();
+        },
+        toggleSlideMenu: function () {
+                $('.que').on('click', function () {
+                        $(this).toggleClass('is_active')
+                        $(this).next('.answer').slideToggle()
+                }).eq(1).trigger("click")
+
+        },
+
+        visualSlide: function () {
+
+        }
+
+};
 
