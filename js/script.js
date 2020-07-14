66e65b956cf09c0c1de4c584a0bf5c93022d940b
Hyeeun Shin <hyeeunshin@Hyeeunui-iMac.local>
2020-07-14 18:51:51 +0900

    ..

--- js/script.js
+++ js/script.js
@@ -1,69 +1,100 @@
 $(function () {
-        /**
-         * marketer UI Script Initialize.
-         */
-        marketer.init();
+    /**
+     * marketer UI Script Initialize.
+     */
+    marketer.init();
 })
 
 const marketer = {
-        init: function () {
-            marketer.toggleSlideMenu();
-            marketer.getScroll();
-            marketer.backTop();
-            marketer.slide();
-            marketer.popup();
-        },
+    init: function () {
+        marketer.toggleSlideMenu();
+        marketer.getScroll();
+        marketer.backTop();
+        marketer.slide();
+        marketer.handleImagePopup();
+        marketer.handleArrows();
+        marketer.setPagination();
+    },
 
-        toggleSlideMenu: function () {
-            $('.panel-item').on('click', function () {
-                $(this).find('.answer').slideDown();
-                $(this).siblings().find('.answer').slideUp();
-            }).eq(1).trigger('click');
+    toggleSlideMenu: function () {
+        $('.panel-item').on('click', function () {
+            $(this).find('.answer').slideDown();
+            $(this).siblings().find('.answer').slideUp();
+        }).eq(1).trigger('click');
 
-            $('.btn-menu').on('click', function () {
-                $(this).prev('nav').slideToggle();
-            });
-        },
+        $('.btn-menu').on('click', function () {
+            $(this).prev('nav').slideToggle();
+        });
+    },
 
 
-        getScroll: function () {
-            $(window).on('scroll', function () {
-                const scrollTop = $(Window).scrollTop();
-                if (scrollTop > 300) {
-                    $('html').addClass('show-header')
-                } else {
-                    $('html').removeClass('show-header')
+    getScroll: function () {
+        $(window).on('scroll', function () {
+            const scrollTop = $(Window).scrollTop();
+            if (scrollTop > 300) {
+                $('html').addClass('show-header')
+            } else {
+                $('html').removeClass('show-header')
+            }
+        });
+    },
+
+    backTop: function () {
+        $('.back-top').on('click', function () {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 400)
+        });
+    },
+
+    slide: function () {
+        $(".slide").slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: false,
+            dots: true
+        });
+    },
+
+    handleImagePopup: function () {
+        $('.gallery li').on('click', function () {
+            index = $(this).index();
+            const src = $(this).find('img').attr('src');
+            $('.popup').css({display: 'flex'}).find('img').attr('src', src);
+            setPagination();
+        })
+
+        $('.popup').on('click', function () {
+            $('.popup').hide();
+        })
+    },
+
+    handleArrows: function () {
+        $('.popup i').on('click', function (e) {
+            e.stopPropagation();
+            const length = $('.gallery li').length;
+            const type = $(this).data('type');
+            if (type === 'left') {
+                if (index === 0) {
+                    index = length;
                 }
-            });
-        },
+                index = index - 1;
+                const src = $('.gallery li').eq(index % length).find('img').attr('src');
+                $('.popup').find('img').attr('src', src);
+            }
 
-        backTop: function () {
-            $('.back-top').on('click', function () {
-                $('html, body').animate({
-                    scrollTop: 0
-                }, 400)
-            });
-        },
+            if (type === 'right') {
+                index = index + 1;
+                const src = $('.gallery li').eq(index % length).find('img').attr('src');
+                $('.popup').find('img').attr('src', src);
+                setPagination();
+            }
+        })
+    },
 
-        slide: function () {
-            $(".slide").slick({
-                autoplay: true,
-                autoplaySpeed: 2000,
-                arrows: false,
-                dots: true
-            });
-        },
-
-        popup: function () {
-            const src = $(this).find('img').attr('src')
-            $('.gallery li').on('click', function () {
-                $('.popup').show().find('img').attr('src', src);
-            });
-            $('.screen').on('click', function () {
-                $('.popup').hide();
-            });
-        }
-
-
+    setPagination: function () {
+        const length = $('.gallery li').length;
+        $('.pagination').html(`${index % length + 1}/${length}`)
+    }
 };
 
